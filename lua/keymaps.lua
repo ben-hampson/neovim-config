ceafd56b33e8355b674fa9c8233ee621e379639f
Ben Hampson <email@email.com>
2024-06-11 10:08:09 +0200

    improvement: nvim buffer-next to <leader>bn

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,7 +8,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Unmaps
-vim.api.nvim_set_keymap('n', '<leader>j', '<Nop>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>j', '<Nop>', { noremap = true })
 
 -- Keymaps for better default experience
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -21,12 +21,14 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Buffer
-vim.keymap.set('n', "bn", ":bn<CR>", { silent = true, desc = "Buffer Next"})
-vim.keymap.set('n', "bp", ":bp<CR>", { silent = true, desc = "Buffer Previous"})
-vim.keymap.set('n', "bq", ":Bdelete<CR>", { silent = true, desc = "Buffer Quit"})
+vim.keymap.set('n', "<leader>bn", ":bn<CR>", { silent = true, desc = "[B]uffer [N]ext" })
+vim.keymap.set('n', "<leader>b2n", ":bn 2<CR>", { silent = true, desc = "[B]uffer [N]ext 2" })
+vim.keymap.set('n', "<leader>bp", ":bp<CR>", { silent = true, desc = "[B]uffer [P]revious" })
+vim.keymap.set('n', "<leader>b2p", ":bp 2<CR>", { silent = true, desc = "[B]uffer [P]revious 2" })
+vim.keymap.set('n', "<leader>bq", ":Bdelete<CR>", { silent = true, desc = "[B]uffer [Q]uit" })
 
 -- NvimTree Toggle
-vim.keymap.set('n', "<C-E>", ":NvimTreeToggle<CR>", { silent = true, desc = "NvimTree Toggle"})
+vim.keymap.set('n', "<C-E>", ":NvimTreeToggle<CR>", { silent = true, desc = "NvimTree Toggle" })
 
 -- Remap because we want <C-k> for moving the cursor up one window
-vim.keymap.set('n', "<C-m>", vim.lsp.buf.signature_help, { silent = true, desc = "Signature Help"})
+vim.keymap.set('n', "<C-m>", vim.lsp.buf.signature_help, { silent = true, desc = "Signature Help" })
