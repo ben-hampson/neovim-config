ab040c1d126529a129ad5d189662bcba5e724e0f
Ben Hampson <email@email.com>
2025-07-04 11:23:06 +0200

    add schema validation

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -113,7 +113,31 @@ return {
         capabilities = capabilities
       })
       lspconfig.yamlls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        -- https://www.arthurkoziel.com/json-schemas-in-neovim/
+        settings = {
+          yaml = {
+            validate = true,
+            -- disable the schema store
+            schemaStore = {
+              enable = false,
+              url = "",
+            },
+            -- manually select schemas
+            schemas = {
+              ['https://json.schemastore.org/kustomization.json'] = 'kustomization.{yml,yaml}',
+              ['https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json'] =
+              'docker-compose*.{yml,yaml}',
+              ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] =
+              "argocd-application.yaml",
+            },
+            format = {
+              enable = false
+            },
+            hover = true,
+            completion = true
+          }
+        }
       })
       lspconfig.bashls.setup({
         capabilities = capabilities
