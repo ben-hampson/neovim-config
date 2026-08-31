517665bbc38a6ef160bf878cccf2dc99694545f6
Ben Hampson <email@email.com>
2026-08-31 10:06:44 +0200

    disable avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,73 +1,74 @@
-return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  build = "make",
-  opts = {
-    mappings = {
-      submit = {
-        insert = "<CR>"
-      }
-    },
-    instructions_file = "avante.md",
-    provider = "copilot",
-    providers = {
-      copilot = {
-        -- endpoint = "https://api.anthropic.com",
-        -- Models: https://docs.github.com/en/copilot/concepts/billing/copilot-requests#model-multipliers
-        -- model = "claude-sonnet-4.5",  -- 1x multiplier
-        -- model = "claude-opus-4.6",  -- 3x multiplier, best ofr coding
-        -- model = "gpt-5.1",  -- 0.33x multipler
-        -- model = "gpt-5-mini",  -- Preferred small model. Free
-        model = "gpt-5.3-codex",  -- 1x multiplier
-        -- model = "auto",
-        -- timeout = 30000, -- Timeout in milliseconds
-        -- extra_request_body = {
-        --   temperature = 0.75,
-        --   max_tokens = 20480,
-        },
-      },
-    selection = {
-      enabled = false,
-    },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-
-    -- {
-    --   -- Make sure to set this up properly if you have lazy=true
-    --   'MeanderingProgrammer/render-markdown.nvim',
-    --   opts = {
-    --     file_types = { "markdown", "Avante" },
-    --   },
-    --   ft = { "markdown", "Avante" },
-    -- },
-  },
-}
+return {}
+-- return {
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   version = false, -- Never set this value to "*"! Never!
+--   build = "make",
+--   opts = {
+--     mappings = {
+--       submit = {
+--         insert = "<CR>"
+--       }
+--     },
+--     instructions_file = "avante.md",
+--     provider = "copilot",
+--     providers = {
+--       copilot = {
+--         -- endpoint = "https://api.anthropic.com",
+--         -- Models: https://docs.github.com/en/copilot/concepts/billing/copilot-requests#model-multipliers
+--         -- model = "claude-sonnet-4.5",  -- 1x multiplier
+--         -- model = "claude-opus-4.6",  -- 3x multiplier, best ofr coding
+--         -- model = "gpt-5.1",  -- 0.33x multipler
+--         -- model = "gpt-5-mini",  -- Preferred small model. Free
+--         model = "gpt-5.3-codex",  -- 1x multiplier
+--         -- model = "auto",
+--         -- timeout = 30000, -- Timeout in milliseconds
+--         -- extra_request_body = {
+--         --   temperature = 0.75,
+--         --   max_tokens = 20480,
+--         },
+--       },
+--     selection = {
+--       enabled = false,
+--     },
+--     },
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "MunifTanjim/nui.nvim",
+--       --- The below dependencies are optional,
+--       "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+--       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--       "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--       "stevearc/dressing.nvim", -- for input provider dressing
+--       "folke/snacks.nvim", -- for input provider snacks
+--       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--       "zbirenbaum/copilot.lua", -- for providers='copilot'
+--       {
+--         -- support for image pasting
+--         "HakonHarnes/img-clip.nvim",
+--         event = "VeryLazy",
+--         opts = {
+--           -- recommended settings
+--           default = {
+--             embed_image_as_base64 = false,
+--             prompt_for_file_name = false,
+--             drag_and_drop = {
+--               insert_mode = true,
+--             },
+--             -- required for Windows users
+--             use_absolute_path = true,
+--           },
+--         },
+--       },
+--
+--     -- {
+--     --   -- Make sure to set this up properly if you have lazy=true
+--     --   'MeanderingProgrammer/render-markdown.nvim',
+--     --   opts = {
+--     --     file_types = { "markdown", "Avante" },
+--     --   },
+--     --   ft = { "markdown", "Avante" },
+--     -- },
+--   },
+-- }
